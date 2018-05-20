93ce9db69da3f801db65c757e151ccaf6b945119
BaoLT <bao.ly@schola.tv>
2018-05-20 13:05:30 +0700

    Added android app

---
+++ Ghostx/android/src/com/ghost/x/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ghost.x.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ghost.x.Corex;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Corex(), config);
+	}
+}
---
+++ Ghostx/desktop/src/com/ghost/x/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ghost.x.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ghost.x.Corex;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Corex(), config);
+	}
+}
